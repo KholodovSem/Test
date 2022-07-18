68edc04740d32e415623c949da59abefab44e9b7
Kholodov Sem <kholodovsem@gmail.com>
2022-07-18 18:01:19 +0300

    1

---
+++ src/TypeScript/test.ts
@@ -0,0 +1,19 @@
+// Типы данных в TypeScript
+
+// Число
+let number: number = 4;
+
+// Строка
+let string: string = "Four";
+
+// Буль
+let boolean: boolean = false;
+
+// Undefined
+let u: undefined = undefined;
+
+//Null
+let n: null = null;
+
+// Объект
+let obj: object = { a: 1 };
